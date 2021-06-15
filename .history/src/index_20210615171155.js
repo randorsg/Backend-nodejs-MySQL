0ceb571ec6f5b0578520ba9a2b0d7582ff35e73e
randorsg <randorsamuel@hotmail.com>
2021-06-15 17:47:00 +0200

    creacion bd Madrid
    meetin_hablando

--- .history/src/index_20210615171155.js
+++ .history/src/index_20210615171155.js
@@ -4,9 +4,6 @@ const morgan = require(morgan);
 const app = express();
 app.set('port', process.env.PORT || 4000);
 
-// middleware 
-app.use(morgan('dev'));
-
 app.listen(app.get('port'), () => {
     console.log('server on port', app.get('port'))
 });
