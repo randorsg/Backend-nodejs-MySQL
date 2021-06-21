e2aca2ed729abe628b3ea74e55509efafaf6ae63
randorsg <randorsamuel@hotmail.com>
2021-06-21 09:20:26 +0200

    errores por corregir

--- .history/src/index_20210615171339.js
+++
@@ -1,9 +0,0 @@
-const express = require(express);
-const morgan = require(morgan);
-
-const app = express();
-app.set('port', process.env.PORT || 4000);
-
-app.listen(app.get('port'), () => {
-    console.log('server on port', app.get('port'))
-});
